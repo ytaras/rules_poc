67c598fa6d7e725d6c324aca50c19d549a024e2c
Yura Taras <yura.taras@gmail.com>
2016-04-09 12:37:21 +0300

    Added Try as output

--- src/main/scala/Rule.scala
+++ src/main/scala/Rule.scala
@@ -1,32 +1,53 @@
+import scala.util.Try
+
 /**
   * Created by ytaras on 4/9/16.
   */
 
-case class Rule[In, St, Params, +Out](run: (=> In, => St, => Params) => Out) {
+case class Rule[In, St, Params, +Out](run: (=> In, => St, => Params) => Try[Out]) {
+  def unsafe(a: => In, b: => St, c: => Params) = run(a, b, c).get
   def map[BOut](f: Out => BOut): Rule[In, St, Params, BOut] =
-    Rule { (a, b, c) => f(run(a, b, c)) }
+    Rule { (a, b, c) => run(a, b, c).map(f) }
 
   def flatMap[BOut](f: Out => Rule[In, St, Params, BOut]): Rule[In, St, Params, BOut] = Rule {
-    (a, b, c) => f(run(a, b, c)).run(a, b, c)
+    (a, b, c) => run(a, b, c).map(f).flatMap(_.run(a, b, c))
   }
 
 }
 
 object Rule {
   def fromInput[A, B](f: A => B): Rule[A, Any, Any, B] =
-    Rule { (a, _, _) => f(a) }
+    Rule { (a, _, _) => Try {
+      f(a)
+    }
+    }
   def fromState[A, B](f: A => B): Rule[Any, A, Any, B] =
-    Rule { (_, a, _) => f(a) }
+    Rule { (_, a, _) => Try {
+      f(a)
+    }
+    }
   def fromParam[A, B](f: A => B): Rule[Any, Any, A, B] =
-    Rule { (_, _, a) => f(a) }
+    Rule { (_, _, a) => Try {
+      f(a)
+    }
+    }
 
   def pure[A](v: => A): Rule[Any, Any, Any, A] =
-    Rule { (_, _, _) => v }
+    Rule { (_, _, _) => Try {
+      v
+    }
+    }
+
+  def fromPure[A, B, C, D](f: (A, B, C) => D): Rule[A, B, C, D] = Rule {
+    (a, b, c) => Try {
+      f(a, b, c)
+    }
+  }
+
 
   implicit class FunctionalRuleSyntax[A, B](r: Rule[Any, Any, Any, A => B]) {
-    def ap(b: Rule[Any, Any, Any, A]): Rule[Any, Any, Any, B] = Rule {
-      (i, s, p) => r.run(i, s, b).apply(b.run(i, s, b))
-    }
+    def ap(b: Rule[Any, Any, Any, A]): Rule[Any, Any, Any, B] =
+      r.flatMap { f => b.map(f) }
   }
 }
 
--- src/test/scala/RulesSpec.scala
+++ src/test/scala/RulesSpec.scala
@@ -8,41 +8,41 @@ class RulesSpec extends Specification with ScalaCheck {
   val pInput = Prop.forAll { (a: Int, b: Int) =>
     Rule.fromInput[Int, Int] {
       _ + a
-    }.run(b, notRead, notRead) === a + b
+    }.unsafe(b, notRead, notRead) === a + b
   }
   val pState = Prop.forAll { (a: Int, b: Int) =>
     Rule.fromState[Int, Int] {
       _ + a
-    }.run(notRead, b, notRead) === a + b
+    }.unsafe(notRead, b, notRead) === a + b
   }
   val pParam = Prop.forAll { (a: Int, b: Int) =>
     Rule.fromParam[Int, Int] {
       _ + a
-    }.run(notRead, notRead, b) === a + b
+    }.unsafe(notRead, notRead, b) === a + b
   }
   val pAll = Prop.forAll { (a: Int, b: Int, c: Int) =>
-    Rule[Int, Int, Int, Int] {
+    Rule.fromPure[Int, Int, Int, Int] {
       _ + _ + _
-    }.run(a, b, c) === a + b + c
+    }.unsafe(a, b, c) === a + b + c
   }
   val pPure = Prop.forAll {
-    (a: Int) => Rule.pure(a).run(notRead, notRead, notRead) === a
+    (a: Int) => Rule.pure(a).unsafe(notRead, notRead, notRead) === a
   }
   val pMap = Prop.forAll { (a: Int, b: Int) =>
     Rule.pure(a)
-      .map(_ * b).run(notRead, notRead, notRead) === a * b
+      .map(_ * b).unsafe(notRead, notRead, notRead) === a * b
   }
   val pFlatMap = Prop.forAll { (a: Int, b: Int) =>
     val r = for {
       in1 <- Rule.pure(a)
       in2 <- Rule.pure(b)
     } yield in1 * in2
-    r.run(notRead, notRead, notRead) === a * b
+    r.unsafe(notRead, notRead, notRead) === a * b
   }
 
   val pAp = Prop.forAll { (a: Int, b: Int) =>
     Rule.pure[Int => Int](_ + a).ap(Rule.pure(b))
-      .run(notRead, notRead, notRead) === a + b
+      .unsafe(notRead, notRead, notRead) === a + b
   }
 
   def is =
