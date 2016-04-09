b1dcba354f75825f02fe06c9972b594836b6cf70
Yura Taras <yura.taras@gmail.com>
2016-04-09 10:02:05 +0300

    Rule monad instance

--- src/main/scala/Rule.scala
+++ src/main/scala/Rule.scala
@@ -2,7 +2,15 @@
   * Created by ytaras on 4/9/16.
   */
 
-case class Rule[-In, -St, -Params, +Out](run: (In, St, Params) => Out)
+case class Rule[In, St, Params, +Out](run: (=> In, => St, => Params) => Out) {
+  def map[BOut](f: Out => BOut): Rule[In, St, Params, BOut] =
+    Rule { (a, b, c) => f(run(a, b, c)) }
+
+  def flatMap[BOut](f: Out => Rule[In, St, Params, BOut]): Rule[In, St, Params, BOut] = Rule {
+    (a, b, c) => f(run(a, b, c)).run(a, b, c)
+  }
+
+}
 
 object Rule {
   def fromInput[A, B](f: A => B): Rule[A, Any, Any, B] =
@@ -11,5 +19,8 @@ object Rule {
     Rule { (_, a, _) => f(a) }
   def fromParam[A, B](f: A => B): Rule[Any, Any, A, B] =
     Rule { (_, _, a) => f(a) }
+
+  def pure[A](v: => A): Rule[Any, Any, Any, A] =
+    Rule { (_, _, _) => v }
 }
 
--- src/test/scala/RulesSpec.scala
+++ src/test/scala/RulesSpec.scala
@@ -4,25 +4,56 @@ import org.specs2.{ScalaCheck, Specification}
 /**
   * Created by ytaras on 4/9/16.
   */
-class RulesSpec extends Specification with ScalaCheck { def is = s2"""
+class RulesSpec extends Specification with ScalaCheck {
+  val pInput = Prop.forAll { (a: Int, b: Int) =>
+    Rule.fromInput[Int, Int] {
+      _ + a
+    }.run(b, notRead, notRead) === a + b
+  }
+  val pState = Prop.forAll { (a: Int, b: Int) =>
+    Rule.fromState[Int, Int] {
+      _ + a
+    }.run(notRead, b, notRead) === a + b
+  }
+  val pParam = Prop.forAll { (a: Int, b: Int) =>
+    Rule.fromParam[Int, Int] {
+      _ + a
+    }.run(notRead, notRead, b) === a + b
+  }
+  val pAll = Prop.forAll { (a: Int, b: Int, c: Int) =>
+    Rule[Int, Int, Int, Int] {
+      _ + _ + _
+    }.run(a, b, c) === a + b + c
+  }
+  val pPure = Prop.forAll {
+    (a: Int) => Rule.pure(a).run(notRead, notRead, notRead) === a
+  }
+  val pMap = Prop.forAll { (a: Int, b: Int) =>
+    Rule.pure(a)
+      .map(_ * b).run(notRead, notRead, notRead) === a * b
+  }
+  val pFlatMap = Prop.forAll { (a: Int, b: Int) =>
+    val r = for {
+      in1 <- Rule.pure(a)
+      in2 <- Rule.pure(b)
+    } yield in1 * in2
+    r.run(notRead, notRead, notRead) === a * b
+  }
+
+  def is =
+    s2"""
      Rules use input: $pInput
      Rules use state: $pState
      Rules use params: $pParam
      Rules can use everything: $pAll
+
+     Rule is a monad:
+     Can return: $pPure
+     Can map: $pMap
+     Can flatMap: $pFlatMap
     """
 
-  // TODO - Reduce boilderplate
-  val pInput = Prop.forAll { (a: Int, b: Int) =>
-    Rule.fromInput[Int, Int] { _ + a }.run(b, None, None) === a + b
-  }
-  val pState = Prop.forAll { (a: Int, b: Int) =>
-    Rule.fromState[Int, Int] { _ + a}.run(None, b, None) === a + b
-  }
-  val pParam = Prop.forAll { (a: Int, b: Int) =>
-    Rule.fromParam[Int, Int] { _ + a}.run(None, None, b) === a + b
-  }
-  val pAll = Prop.forAll { (a: Int, b: Int, c: Int) =>
-    Rule[Int, Int, Int, Int] { _ + _ + _}.run(a, b, c) === a + b + c
-  }
+  //noinspection NotImplementedCode
+  def notRead = ???
 
 }
